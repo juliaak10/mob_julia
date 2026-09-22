7dbefa08b296752ced188875a2c9b7db07602065
juliaak10 <julia.nicolack@escola.pr.gov.br>
2026-09-22 21:12:55 +0000

    exercicio 4 ok

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 const eu = {
   nome: "Gaby",
   idade: 17,
-  cidade: "Cascavel"
+  cidade: "Cascavel",
+  nota1: 8,
+  nota2: 9
 };
+
+function calcularMedia(pessoa) {
+  return (pessoa.nota1 + pessoa.nota2) / 2;
+}
 
 export default function App() {
   return (
@@ -12,6 +18,7 @@ export default function App() {
       <Text>{eu.nome}</Text>
       <Text>{eu.idade}</Text>
       <Text>{eu.cidade}</Text>
+      <Text>Média: {calcularMedia(eu)}</Text>
     </View>
   );
 }
